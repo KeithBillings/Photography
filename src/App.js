3c1eca101f63cb75d232f663f1f4f4187633e3db
Keith Billings <keith.billings@brandlive.com>
2021-06-02 23:23:30 -0700

    create light and dark theme, finish portfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import { useState } from 'react';
+
 import NavBar from './components/Navbar/Navbar';
 import Home from './Pages/Home';
 
 import './App.scss';
 
 function App() {
+	const [theme, setTheme] = useState('light');
+
+	function handleChangeTheme (e) {
+		const mainParent = document.getElementsByClassName('App')[0];
+		const themeBtn = document.getElementsByClassName('theme-button')[0];
+		const customSeperator = document.getElementsByClassName('custom-seperator')[0];
+		const bookASessionSVG = document.querySelector("#about > div.about-content > div.about-content__text > a > svg");
+		
+		if (theme === 'light'){
+			setTheme('dark');
+			
+			mainParent.classList.remove('theme-light');
+			mainParent.classList.add('theme--dark');
+			
+			themeBtn.textContent = 'Light mode';
+
+			customSeperator.style.borderColor = 'white';
+			bookASessionSVG.style.fill = 'white';
+		}
+		if (theme === 'dark'){
+			setTheme('light');
+			
+			mainParent.classList.remove('theme--dark');
+			mainParent.classList.add('theme--light');
+			
+			themeBtn.textContent = 'Dark mode';
+
+			customSeperator.style.borderColor = 'black';
+			bookASessionSVG.style.fill = 'black';
+		}
+	}
+
   return (
     <div className='App'>
+			<button className='theme-button' onClick={handleChangeTheme}>Dark mode</button>
       <NavBar />
       <Home />
       {/* <Footer /> */}
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,83 @@
-export default function Portfolio () {
+import image1 from '../../assets/images/portfolio/1.jpg';
+import image2 from '../../assets/images/portfolio/2.jpg';
+import image3 from '../../assets/images/portfolio/3.jpg';
+import image4 from '../../assets/images/portfolio/4.jpg';
+import image5 from '../../assets/images/portfolio/5.jpg';
+import image6 from '../../assets/images/portfolio/6.jpg';
+import image7 from '../../assets/images/portfolio/7.jpg';
+import image8 from '../../assets/images/portfolio/8.jpg';
+import image9 from '../../assets/images/portfolio/9.jpg';
+import image10 from '../../assets/images/portfolio/10.jpg';
+
+export default function Portfolio() {
+  const arrayOfImages = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10];
+
+  function getRandomInt() {
+    return Math.floor(Math.random() * 3);
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    let lazyloadImages;
+
+    if ('IntersectionObserver' in window) {
+      lazyloadImages = document.querySelectorAll('.lazy');
+      let imageObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+          if (entry.isIntersecting) {
+            let image = entry.target;
+            image.src = image.dataset.src;
+						image.classList.add('fade-in');
+            image.classList.remove('lazy');
+            imageObserver.unobserve(image);
+          }
+        });
+      });
+
+      lazyloadImages.forEach(function (image) {
+        imageObserver.observe(image);
+      });
+    } else {
+      let lazyloadThrottleTimeout;
+      lazyloadImages = document.querySelectorAll('.lazy');
+
+      function lazyload() {
+        if (lazyloadThrottleTimeout) {
+          clearTimeout(lazyloadThrottleTimeout);
+        }
+
+        lazyloadThrottleTimeout = setTimeout(function () {
+          let scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(function (img) {
+            if (img.offsetTop < window.innerHeight + scrollTop) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+          });
+          if (lazyloadImages.length === 0) {
+            document.removeEventListener('scroll', lazyload);
+            window.removeEventListener('resize', lazyload);
+            window.removeEventListener('orientationChange', lazyload);
+          }
+        }, 20);
+      }
+
+      document.addEventListener('scroll', lazyload);
+      window.addEventListener('resize', lazyload);
+      window.addEventListener('orientationChange', lazyload);
+    }
+  });
+
   return (
-		<div id='portfolio'>
-			portfolio
-		</div>
-	)
+    <div id='portfolio' className='portfolio-section'>
+      {arrayOfImages.map((image, index) => {
+        let tallOrWide = '';
+
+        if (getRandomInt() === 1) {
+          tallOrWide = 'tall';
+        }
+
+        return <img data-src={image} loading='lazy' alt={`${image} ${index}`} key={index} className={`lazy ${tallOrWide}`} />;
+      })}
+    </div>
+  );
 }
